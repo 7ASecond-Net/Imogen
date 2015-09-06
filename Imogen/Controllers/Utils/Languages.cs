83265e2bc3db41dac11239fc4fe5c75a064d7d4d
Dave Gordon <admin@7asecond.net>
2015-09-06 14:54:58 +0100

    Added Language Collection

---
+++ Imogen/Controllers/Utils/Languages.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imogen.Controllers.Utils
+{
+    class Languages
+    {
+
+        public List<string> GetAvailableLanguages()
+        {
+            List<string> allLanguages = new List<string>();
+            CultureInfo[] cis = CultureInfo.GetCultures(CultureTypes.AllCultures);
+
+            foreach(CultureInfo ci in cis)
+            {
+                allLanguages.Add(ci.EnglishName);
+            }
+
+            return allLanguages;
+        }
+
+    }
+}
