f6815b090c1353bb423736cf14eebec99d327424
Dave Gordon <admin@7asecond.net>
2015-09-06 14:54:36 +0100

    Added Gender Collection

---
+++ Imogen/Controllers/Utils/Gender.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imogen.Controllers.Utils
+{
+    class Gender
+    {
+
+        //TODO: Un-Hard-Code this
+        internal List<string> GetAllSexes()
+        {
+            List<string> tmpList = new List<string>();
+
+            tmpList.Add("Male");
+            tmpList.Add("Female");
+            tmpList.Add("Transgender Male");
+            tmpList.Add("Transgender Female");
+            tmpList.Add("Hermaphrodite");
+            tmpList.Add("Unknown");
+
+
+            return tmpList;
+        }
+    }
+}
